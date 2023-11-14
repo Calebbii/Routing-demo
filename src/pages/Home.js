ff62ab8b51d969a02aba6fb61feba7b38fb100a6
Calebbii <biicaleb316@gmail.com>
2023-11-14 10:10:57 +0300

    initial route setup

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Header from '../components/Header'
+import Navbar from '../components/Navbar'
+
+function Home() {
+  return (
+    <div>
+        <Navbar/>
+        <Header/>
+        <h1>This is the home page</h1>
+    </div>
+  )
+}
+
+export default Home
