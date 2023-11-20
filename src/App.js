460d644cdb49e60126165733d070039f8952fc2b
Calebbii <biicaleb316@gmail.com>
2023-11-20 09:41:40 +0300

    created the testimonials component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Contact from './pages/Contact';
 import Home from './pages/Home';
 import About from './pages/About';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Testimonials from './pages/Testimonials';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
         <Route path='/contact' element={<Contact/>}/>
+        <Route path='/tes' element={<Testimonials/>}/>
       </Routes>
       </BrowserRouter>
       
